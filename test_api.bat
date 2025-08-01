876ba2b806accde3f39852895594fb17a531b331
EdwinVh02 <skrillexelmejor77@gmail.com>
2025-07-31 20:03:44 -0600

    todo

--- test_api.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set TOKEN=70^|zEXTpGVbFr60yuqo1SpqAQcKKKF9MX8GBTvVsyO66bd55a17
-curl -X GET "http://localhost:8000/api/debug-headers" -H "Authorization: Bearer %TOKEN%" -H "Content-Type: application/json" -H "Accept: application/json"
